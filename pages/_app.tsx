875d123aecb98d4ee4123890fc2691cd76927b80
sorakim <sora6418@gmail.com>
2023-10-14 19:10:27 +0900

    chore: 프로젝트 세팅 (prettier, emotion, reset style 추가)

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import HomePage from "@/pages/index";
+import { GlobalStyle } from "@/src/components/styles/GlobalStyle";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <GlobalStyle />
+      <Component {...pageProps}>
+        <HomePage />
+      </Component>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+const HomePage = () => {
+
+  return (
+    <Container>
+        page!!!!!!!!!!!!!!!!!
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  
+`;
+
+
+export default HomePage
