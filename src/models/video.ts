08c0ac2c9abd1190b7df39da7b0ee1b998a30dbc
sorakim <sora6418@gmail.com>
2023-10-30 09:31:56 +0900

    feat: vidoes api 연동

---
+++ src/models/video.ts
@@ -0,0 +1,77 @@
+export interface VideosResponse {
+  kind: string;
+  etag: string;
+  items: VideosItem[];
+  nextPageToken: string;
+  pageInfo: VideosPageInfo;
+}
+
+export interface VideosItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  defaultAudioLanguage: string;
+  defaultLanguage?: string;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium: Medium;
+  high: High;
+  standard: Standard;
+  maxres?: Maxres;
+}
+
+export interface Default {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Medium {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface High {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Standard {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Maxres {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Localized {
+  title: string;
+  description: string;
+}
+
+export interface VideosPageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
