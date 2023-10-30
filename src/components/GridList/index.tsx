0c72a13178d2b049847baf34619297adee9e1df0
sorakim <sora6418@gmail.com>
2023-10-30 19:49:34 +0900

    feat: 그리드 리스트가 col, row 갭을 구분할 수 있도록 수정

--- src/components/GridList/index.tsx
+++ src/components/GridList/index.tsx
@@ -4,19 +4,26 @@ import React from "react";
 interface Props {
   children: React.ReactNode;
   cols: number;
-  gap?: number;
+  gap?: number[] | number;
 }
+
 const GirdList = ({ cols, gap = 0, children }: Props) => {
+  const [columGap, rowGap] = Array.isArray(gap) ? gap : [gap];
   return (
-    <Container cols={cols} gap={gap}>
+    <Container cols={cols} columGap={columGap} rowGap={rowGap ?? columGap}>
       {children}
     </Container>
   );
 };
 
-const Container = styled.div<{ cols: number; gap: number }>`
+const Container = styled.div<{
+  cols: number;
+  columGap: number;
+  rowGap: number;
+}>`
   display: grid;
-  gap: ${({ gap }) => gap}px;
+  column-gap: ${({ columGap }) => columGap}px;
+  row-gap: ${({ rowGap }) => rowGap}px;
   grid-template-columns: repeat(${({ cols }) => cols}, 1fr);
 `;
 
